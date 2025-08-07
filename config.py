6af434177a06667d4b31fd6567d92132fe4e2c0b
nitro01a1 <amirrp4090@gmail.com>
2025-08-08 00:15:17 +0330

    config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ ADMIN_ID = 8066854428  # آیدی عددی خودتان را اینجا قرار
 FORCED_JOIN_CHANNELS = ["@x7gap", "@npvpnir"]
 
 # لینک وب‌سایت شما در Render (بعد از ساخت پروژه در Render این را کامل کنید)
-WEBHOOK_URL = "https://likebot.onrender.com"
+WEBHOOK_URL = "https://likebot-q6aj.onrender.com"
 
 # پیام خطای ثانویه (توسط ادمین فعال/غیرفعال می‌شود)
 SECONDARY_ERROR_ENABLED = False
