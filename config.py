e7e0ef32f9910d6f48bccfeb719f0e5be426fb4a
nitro01a1 <amirrp4090@gmail.com>
2025-08-08 00:10:45 +0330

    config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# config.py
+
+# توکن ربات که از BotFather دریافت می‌کنید
+BOT_TOKEN = "7882319394:AAG-TFTzkcEccTbR3sEIOJ0I9StWJMhNeHc"
+
+# آیدی عددی اکانت ادمین
+ADMIN_ID = 8066854428  # آیدی عددی خودتان را اینجا قرار دهید
+
+# آیدی کانال‌هایی که کاربر باید عضو شود (با @)
+# ربات باید در این کانال‌ها ادمین باشد
+FORCED_JOIN_CHANNELS = ["@x7gap", "@npvpnir"]
+
+# لینک وب‌سایت شما در Render (بعد از ساخت پروژه در Render این را کامل کنید)
+WEBHOOK_URL = "https://likebot.onrender.com"
+
+# پیام خطای ثانویه (توسط ادمین فعال/غیرفعال می‌شود)
+SECONDARY_ERROR_ENABLED = False
+SECONDARY_ERROR_MESSAGE = "خطا❌ در اتصال به سرور مشکلی پیش آمده با مدیریت تماس بگیرید."
+
+# اطلاعات پشتیبانی
+OWNER_ID = "@immmdold"
+SUPPORT_ID = "@likeadminx7"
